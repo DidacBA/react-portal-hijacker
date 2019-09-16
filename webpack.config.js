a6803c2303d5f22e8ac02afe3ac79a7541af6c98
Didac Bigorda <didacbigorda@gmail.com>
2019-09-16 19:41:15 -0400

    Webpack set up

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './lib'),
+    filename: 'react-portal-hijacker.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.(js)$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
+      }
+    ]
+  },
+  watch: true,
+}
